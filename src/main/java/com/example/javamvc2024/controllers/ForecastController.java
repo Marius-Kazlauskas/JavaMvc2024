6b4cb6a0256b4b35f145c3785bdb525e528106e8
LAPTOP-IHFLTACV\daiva <mar.kazlauskas@gmail.com>
2024-03-12 09:54:58 +0200

    Hello world

---
+++ src/main/java/com/example/javamvc2024/controllers/ForecastController.java
@@ -0,0 +1,15 @@
+package com.example.javamvc2024.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ForecastController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
+}
